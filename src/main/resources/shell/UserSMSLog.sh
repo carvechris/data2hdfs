4d0ed146861606933809b4738900387cc902b0ca
tomaer <i@tomaer.com>
2018-10-18 14:35:23 +0800

    切分脚本

--- src/main/resources/shell/UserSMSLog.sh
+++ src/main/resources/shell/UserSMSLog.sh
@@ -3,18 +3,17 @@
 # author: tomaer.Ma
 
 # 获取当天日期 & 昨天日期
-DATE=`date "+%Y-%m-%d"`
-YESTERDAY=`date -d "1 day ago" +"%Y-%m-%d"`
-hadoop fs -rm -R /tmp/storm/UserSMSLog/${DATE}
-hadoop fs -mkdir -p /tmp/storm/UserSMSLog/${DATE}
-
-# hadoop fs -chown hdfs:hadoop /tmp/storm/UserSMSLog/${DATE}
-# hadoop fs -chmod 777 /tmp/storm/UserSMSLog/${DATE}
 
 # 打印出当前日期下的所有的bolt切分过的文件
-for file in `hadoop fs -ls /user/storm/UserSMSLog/${DATE} |grep /user/storm |awk '{print $8}'`
+for file in `hadoop fs -ls /user/storm/hdfs |grep /user/storm |awk '{print $8}'`
 do
-    filename=${file##*/}
-    hadoop fs -cp $file /tmp/storm/UserSMSLog/${DATE}/${filename}
-    hive -e "LOAD DATA INPATH '/tmp/storm/UserSMSLog/${DATE}/${filename}' into table storm.usersmslog partition (dt='${DATE}')"
+    echo $file >>~/hdfs.tmp
+done
+sort ~/hdfs.tmp >>~/hdfs.tmp.sorted
+rm -rf ~/hdfs.tmp
+sed -i '$d' ~/hdfs.tmp.sorted
+
+for line in `cat ~/hdfs.tmp.sorted`
+do
+    echo $line
 done
